f635af5f9e3af7df1520d2a38081afb1cfc1a4ef
tardelliaraujo <tardelli.blues@gmail.com>
2023-09-25 17:39:04 -0300

    condicionais

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,21 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        switch (plano){
+            case "T":{
+                System.out.println("5Gb YouTube");
+
+            }
+            case "M":{
+                System.out.println("Whats e Instagram grátis");
+                
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+                
+            }
+
+        }
+    }
+}
